4af696f04821a686904e2b669c956416d9fcb2a3
Trillion_Impulse <tkdduq2897@naver.com>
2025-10-24 19:31:05 +0900

    feat(app.py): 메인 페이지 라우터 추가, 유저 등록 라우터 삭제, 게시글 포스팅 라우터 추가, 게시글 조회 라우터로 변경

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from flask_mysqldb import MySQL
 import config
 
@@ -12,21 +12,41 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
-@app.route('/add')
-def add_user():
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/add', methods=['POST'])
+def add_post():
+    # 브라우저에서 데이터 가져오기
+    UserName = request.form.get('user-name')
+    Content = request.form.get('content')
+    
+    # 유효성 검사
+    if not UserName or not Content:
+        return "이름과 내용을 모두 입력해주세요",400
+    if len(UserName)>12:
+        return "이름은 12자 이하로 입력해주세요",400
+    if len(Content)>500:
+        return "내용은 500자 이하로 입력해주세요",400
+
+    # DB 저장
     cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO users (name) VALUES ('홍길동')")
+    cur.execute("INSERT INTO posts (name, content) VALUES (%s, %s)",(UserName, Content))
     mysql.connection.commit()
     cur.close()
-    return "홍길동 추가 완료!"
+    return f"{UserName}의 게시물 포스팅 완료"
 
-@app.route('/users')
-def select_users():
+@app.route('/posts')
+def select_posts():
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM users")
+
+    # 작성일 기준으로 내림차순 조회
+    cur.execute("SELECT * FROM posts ORDER BY created_at DESC")
+
     rows = cur.fetchall()
     cur.close()
-    return {'users': rows}
+    return {'posts': rows}
 
 if __name__ == '__main__':
     app.run(debug=True)
