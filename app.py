1b951bdaeda3ce3e294fd9f95a01f02e8c3f13d2
Trillion_Impulse <tkdduq2897@naver.com>
2025-10-27 19:30:13 +0900

    feat(app.py): 사용자 객체 정의 및 로그인 사용자 로더 추가

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, redirect, url_for, flash
 from flask_mysqldb import MySQL
 import config
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
 
 app = Flask(__name__)
 app.secret_key = config.SECRET_KEY
@@ -12,6 +14,31 @@ app.config['MYSQL_DB'] = config.MYSQL_DB
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
+
+# 비밀번호 해시 생성/검증 기능 초기화
+bcrypt = Bcrypt(app)
+
+# 로그인 기능 초기화
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# 유저 객체
+class User(UserMixin):
+    def __init__(self, UserId, UserName, UserEmail):
+        self.id=UserId
+        self.name = UserName
+        self.email = UserEmail
+
+@login_manager.user_loader
+def load_user(UserId):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users WHERE user_id=%s", (UserId,))
+    UserData = cur.fetchone()
+    cur.close()
+    if UserData:
+        return User(UserData['user_id'], UserData['user_name'], UserData['email'])
+    return None
 
 @app.route('/')
 def main():
